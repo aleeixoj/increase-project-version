6705d8b0b086ddd45946d13a436e923c5a6c2391
aleixo <aleixo@pop-os.localdomain>
2022-06-28 13:46:21 -0300

    1.1.0

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,31 @@ export function increaseVersion(packageJsonPath: string): void {
   pkg.split(/\r?\n/).forEach((line: string) => {
     if (line.includes('"version":')) {
       const answer = readlineSync.question(
-        'Essa compilação é \n[1] - correção de bugs ou [2] - versão estavel?'
+        `This compilation is
+        [1] - Stable version
+        [2] - Hard update
+        [3] - Soft update
+        [4] - To cancel\n`
       );
 
       // eslint-disable-next-line prefer-const
-      let [lts, control, bug] = line.substring(14, line.length - 2).split('.');
+      let [lts, hard, soft] = line.substring(14, line.length - 2).split('.');
 
       if (answer === '1') {
-        bug = (Number(bug) + 1).toString();
+        lts = (Number(lts) + 1).toString();
+        hard = '0';
+        soft = '0';
       }
       if (answer === '2') {
-        lts = (Number(lts) + 1).toString();
+        hard = (Number(hard) + 1).toString();
+        soft = '0';
+      }
+      if (answer === '3') {
+        soft = (Number(soft) + 1).toString();
       }
 
       // eslint-disable-next-line no-param-reassign
-      line = `  "version": "${lts}.${control}.${bug}",`;
+      line = `  "version": "${lts}.${hard}.${soft}",`;
     }
     newPkg.push(line);
   });
